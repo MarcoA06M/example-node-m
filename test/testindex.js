abdb9566b4cb0f02ce430448c97a5d4df034023f
Marco Marquez <mmarquez@lycee.local>
2025-10-07 22:18:55 -0600

    cambios finales

--- test/testindex.js
+++ test/testindex.js
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
 function hash(mensaje) {
-    const hash = crypto.createHash('sha512');
+    const hash = crypto.createHash('sha256');
     hash.update(mensaje);
     return hash.digest('hex');
 }
